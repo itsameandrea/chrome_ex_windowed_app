5fc88c0e44d9293491308c758d08ae65f287e217
ilrock <andrea.rocca3@gmail.com>
2018-12-07 06:09:28 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Welcome!
+
+This extension boilerplate will allow you to run any website as a Chrome Extension. I like this better because I can have a website assigned to a window rather than a Chrome tab.
+
+To Set up your own extension follow the steps below
+======
+
+1. Clone this repo on your device (I have it in my linux distro)
+1. Open view.html and replace ##YOUR_WEBSITE_NAME## with the name of the website you want to window
+2. Open background.js and replace ##ADD_YOUR_URL_HERE## with the url of the website you want to window
+3. Open manifest.json and replace ##YOUR_WEBSITE_NAME## and ##YOUR_WEBSITE_DESCRIPTION## with the name and description of the website you want to window
+4. replace icon_16.png and icon_128.png with the icon of the website you want to window (one icon to be 16x16px and the other one to be 128x128px)
+5. Go to chrome://extensions and enable Developer mode
+6. Click on Load Unpacked and select the app folder within this repo
+7. Enjoy your windowed app
---
+++ app/background.js
@@ -0,0 +1,51 @@
+// Constants
+var baseUrl = "##ADD_YOUR_URL_HERE##";
+var view = "view.html";
+var width = 1100;
+var height = 700;
+var minWidth = 765;
+var minHeight = 300;
+
+// Setup the initial website window
+chrome.app.runtime.onLaunched.addListener(function() {
+    createWindow(baseUrl);
+});
+
+function createWindow(destUrl) {
+    chrome.app.window.create(
+        view, {
+            // Set the height and width and then render it in the middle of the screen
+            innerBounds: {
+                width: width,
+                height: height,
+                minWidth: minWidth,
+                minHeight: minHeight,
+                left: Math.round((screen.availWidth - width) / 2),
+                top: Math.round((screen.availHeight - height) / 2)
+            }
+        }, function(createdWindow) {
+            createdWindow.contentWindow.onload = function() {
+                // Retrieve the webview element
+                var webview = createdWindow.contentWindow.document.querySelector('webview');
+                // When the window is resized, resize the webview to conform to the new size
+                createdWindow.onBoundsChanged.addListener(function() {
+                    var bounds = createdWindow.getBounds();
+                    webview.style.height = bounds.height;
+                    webview.style.width = bounds.width;
+                });
+                // Render the page requested inside the webview
+                webview.src = destUrl;
+                // Add event listener for when webview finishes loading
+                webview.addEventListener("contentload", function() {
+                    // Focus on the webview, so focus can go to the proper elements within it
+                    webview.focus();
+                    // Add event listener for when window becomes focused
+                    createdWindow.contentWindow.onfocus = function() {
+                        // Focus on the webview, so focus can go to the proper elements within it
+                        webview.focus();
+                    };
+                });
+            };
+        }
+    );
+}
